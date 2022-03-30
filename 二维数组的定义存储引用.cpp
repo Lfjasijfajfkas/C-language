761acb8e4147c3c0c805c2140a6809eb27f02360
Lfjasijfajfkas <101780999+Lfjasijfajfkas@users.noreply.github.com>
2022-03-30 17:21:11 +0800

    Add files via upload

---
+++ 二维数组的定义存储引用.cpp
@@ -0,0 +1,31 @@
+//#include<stdio.h>
+
+/*
+//二维数组的定义
+//行数和列数最好用宏定义的形式
+#define M 3
+#define N 4
+int main()
+{
+	int a[M][N];//M行N列
+	return 0;
+}
+*/
+
+/*
+//二维数组的存储
+//二维数组占用多个连续的内存空间
+//总字节数=sizeof(数据类型)*行数*列数
+#define M 3
+#define N 4
+int main()
+{
+	int a[M][N];//M行N列
+	printf("总字节数：%d\n",sizeof(a));
+	return 0;
+}*/
+
+
+//二维数组的引用
+//方法同一维数组，区别在于双下标
+
